fe0afa809c395269fb45738a7815e7def7d0e2f9
ElsongMaster <elsongmaster@gmail.com>
2021-06-24 10:14:42 +0200

    exercice6 partie 4

--- exercice6/src/composants/Header.js
+++ exercice6/src/composants/Header.js
@@ -6,26 +6,29 @@ class Header extends React.Component {
     counter2: 1,
     counter3: 1,
   };
-  handleClickbtn1() {
+  handleClickbtn1(e) {
     this.setState((prevState, props) => ({
       counter1: prevState.counter1 + 1,
     }));
 
+    console.log(`btn: ${e.target.textContent}`);
     console.log(` j'ai été cliqué ${this.state.counter1} fois`);
   }
 
-  handleClickbtn2() {
+  handleClickbtn2(e) {
     this.setState((prevState, props) => ({
       counter2: prevState.counter2 + 1,
     }));
+    console.log(`btn: ${e.target.textContent}`);
 
     console.log(` j'ai été cliqué ${this.state.counter2} fois`);
   }
 
-  handleClickbtn3() {
+  handleClickbtn3(e) {
     this.setState((prevState, props) => ({
       counter3: prevState.counter3 + 1,
     }));
+    console.log(`btn: ${e.target.textContent}`);
 
     console.log(` j'ai été cliqué ${this.state.counter3} fois`);
   }
@@ -33,9 +36,11 @@ class Header extends React.Component {
     return (
       <div>
         <nav>
-          <button onClick={(e) => this.handleClickbtn1()}>Acceuil</button>
-          <button onClick={(e) => this.handleClickbtn2()}>Heure</button>
-          <button onClick={(e) => this.handleClickbtn3()}>Configuration</button>
+          <button onClick={(e) => this.handleClickbtn1(e)}>Acceuil</button>
+          <button onClick={(e) => this.handleClickbtn2(e)}>Heure</button>
+          <button onClick={(e) => this.handleClickbtn3(e)}>
+            Configuration
+          </button>
         </nav>
         <h1>Voici mon nouveau site</h1>
       </div>
