3e3fdfe2f170f37871617cbe2e882d2bc6200523
ElsongMaster <elsongmaster@gmail.com>
2021-06-24 16:31:00 +0200

    point 20

---
+++ evenements_exos/src/composants/Article.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+class Article extends React.Component {
+  render() {
+    return (
+      <div className="container_section">
+        <div className="container-img">
+          <img src="./img/tartine.png" alt="" />
+        </div>
+        <div className="container-article">
+          <h2>{this.props.title}</h2>
+          <p>
+            Lorem ipsum dolor sit amer, consectetur adspiscing elut? sed do
+            eiusmod tempor incicdidunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam? qui nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia desernt mollut anim id est laborum.
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Article;
--- evenements_exos/src/composants/Section.js
+++ evenements_exos/src/composants/Section.js
@@ -1,46 +1,14 @@
 import React from "react";
 import "./Section.css";
+import Article from "./Article";
 class Section extends React.Component {
   render() {
-    let nombreArticles = 1;
+    let nombreArticles = 2;
     return nombreArticles >= 2 ? (
       <div>
         <section>
-          <div className="container_section">
-            <div className="container-img">
-              <img src="./img/tartine.png" alt="" />
-            </div>
-            <div className="container-article">
-              <h2>Mon premier article</h2>
-              <p>
-                Lorem ipsum dolor sit amer, consectetur adspiscing elut? sed do
-                eiusmod tempor incicdidunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam? qui nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia desernt mollut anim id est laborum.
-              </p>
-            </div>
-          </div>
-
-          <div className="container_section">
-            <div className="container-img">
-              <img src="./img/tartine.png" alt="" />
-            </div>
-            <div className="container-article">
-              <h2>Mon premier article</h2>
-              <p>
-                Lorem ipsum dolor sit amer, consectetur adspiscing elut? sed do
-                eiusmod tempor incicdidunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam? qui nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia desernt mollut anim id est laborum.
-              </p>
-            </div>
-          </div>
+          <Article title="tartines" />
+          <Article title="Coding School" />
         </section>
       </div>
     ) : (
